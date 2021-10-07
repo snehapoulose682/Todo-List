4480da3888b4b3245d913301ca6156e550b8f6cb
Sneha Poulose K <sneha.poulose@rapidvaluesolutions.com>
2021-10-07 13:36:40 +0530

    --changed form id. --added a function to delete tasks from the list. --added a function to check the number of task completed.

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ function addTask() {
   var inputValue = document.getElementById("itemInput").value;
   var textInput = document.createTextNode(inputValue);
   var delButton = document.createElement("button");
+  var checkBox = document.createElement("checkbox");
 
   taskList.className += "task__item"
-  
+
   taskList.appendChild(textInput);
   if (inputValue === '') {
     alert("You did not enter any task!")
@@ -13,12 +14,42 @@ function addTask() {
   else {
     document.getElementById("taskList").appendChild(taskList);
 
+    checkBox.innerHTML = '<input type="checkbox" id="item 1" name="item 1" value="Item 1"><label for="item 1"> </label>'
+    checkBox.className += "checkbox__input"
     delButton.innerHTML = '<i class = "fa fa-trash"></i>';
     delButton.className += "button--state-delete"
     taskList.appendChild(delButton);
+    taskList.appendChild(checkBox);
   }
-  document.getElementById("itemInput").value ="";
+  document.getElementById("itemInput").value = "";
 
+  delButton.addEventListener('click', () => this.deleteTask());
+  checkBox.addEventListener('click', () => this.CheckedTask());
+
+}
+
+function deleteTask() {
+  var closeButton = document.getElementsByClassName("button--state-delete");
+
+  for (var i = 0; i < closeButton.length; i++) {
+    closeButton[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+
+}
+
+function CheckedTask() {
+  var inputElems = document.getElementsByTagName("input");
+  var count = 0;
+
+  for (var i = 0; i < inputElems.length; i++) {
+    if (inputElems[i].type === "checkbox" && inputElems[i].checked === true) {
+      count++;
+      document.getElementById("output").innerHTML = count + " task completed ";
+    }
+  }
 }
 
 
